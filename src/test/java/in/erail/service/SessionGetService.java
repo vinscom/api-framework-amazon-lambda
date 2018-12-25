6b7fc67453b8ac7bece10bc3db5bb2e4a0bc7ace
v <v@v.com>
2018-12-25 17:07:32 +0000

    General refactoring

---
+++ src/test/java/in/erail/service/SessionGetService.java
@@ -0,0 +1,36 @@
+package in.erail.service;
+
+import in.erail.common.FrameworkConstants;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.eventbus.Message;
+
+public class SessionGetService extends RESTServiceImpl {
+
+  private JsonArray mSessions = new JsonArray();
+
+  public SessionGetService() {
+    mSessions.add("S1");
+    mSessions.add("S2");
+    mSessions.add("S3");
+    mSessions.add("S4");
+    mSessions.add("S5");
+  }
+
+  @Override
+  public void process(Message<JsonObject> pMessage) {
+    pMessage
+            .reply(new JsonObject().put(
+                    FrameworkConstants.RoutingContext.Json.BODY, 
+                    getSessions().toString().getBytes()));
+  }
+
+  public JsonArray getSessions() {
+    return mSessions;
+  }
+
+  public void setSessions(JsonArray pSessions) {
+    this.mSessions = pSessions;
+  }
+
+}
